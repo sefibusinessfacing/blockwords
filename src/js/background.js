68c65e370096921ab82a3b21227bcd85d8caa6e1
Sefi Ben Yosef <sefi@businessfacing.com>
2018-01-21 12:40:27 +0200

    Update background.js

--- src/js/background.js
+++ src/js/background.js
@@ -2,21 +2,6 @@ var db = openDatabase('Gumshoe', '1', 'login records', 5 * 1024 * 1024);
 
 
 
-/*chrome.tabs.onUpdated.addListener(function() {  	
-	
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-	chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-		console.log(response.farewell);
-		//alert('keyword = ' +response.farewell);
-		portG.postMessage("Hi Popup.js");
-	  });
-	});
-	
-	
-});
-
-*/
-
 chrome.extension.onConnect.addListener(function(port) {      
       console.log("Connected .....");	  
       port.onMessage.addListener(function(msg) {
@@ -26,35 +11,9 @@ chrome.extension.onConnect.addListener(function(port) {
 		   //alert('keyword = ' +response.farewell);
 		   port.postMessage(response.farewell);
 		});
-		}); 	
-	  
-        //   console.log("message recieved" + msg);
-		   
-          // port.postMessage("Hi Popup.js");
+		}); 		       
       });
  })
-
-
-
-
-
-/*
-function paste(){
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {	
-     var response = "";
-	chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-		response =  response.farewell;				
-	  });
-	 alert  
-});
-var value = localStorage.getItem("value");
-return value;
-}  
-*/
-//chrome.runtime.onMessage.addListener(
-//  function(request, sender, sendResponse) {
-	//keyword	= request.keyword;	
-//});
 
 
 
